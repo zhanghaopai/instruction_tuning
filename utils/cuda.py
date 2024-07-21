5e11a6440f5b63637075745acc86bcc07556204b
Patrick <zhanghaopai@outlook.com>
2024-07-21 22:13:08 +0800

    新增脚本

--- utils/cuda.py
+++ utils/cuda.py
@@ -11,3 +11,4 @@ for i in range(torch.cuda.device_count()):
     print("GPU 编号: {}".format(device))
     print("GPU 名称: {}".format(torch.cuda.get_device_name(i)))
     print("GPU total 容量: {} G".format(torch.cuda.get_device_properties(i).total_memory / 1024 / 1024 / 1024))
+    torch.tensor(0.0).to(device)
