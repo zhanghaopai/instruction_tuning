4c2945ccad4c449a7a0b23a3f77c282efe6c79e2
Patrick <zhanghaopai@outlook.com>
2024-07-21 21:54:01 +0800

    修改utils

---
+++ utils/cuda.py
@@ -0,0 +1,13 @@
+import os
+
+import torch
+
+# 环境原因
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+for i in range(torch.cuda.device_count()):
+    device = torch.device("cuda:" + str(i) if torch.cuda.is_available() else "cpu")
+    print("GPU 编号: {}".format(device))
+    print("GPU 名称: {}".format(torch.cuda.get_device_name(i)))
+    print("GPU total 容量: {} G".format(torch.cuda.get_device_properties(i).total_memory / 1024 / 1024 / 1024))
