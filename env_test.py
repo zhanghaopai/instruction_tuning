e066b1290a41a788f3bef0bd934f86218ade3f59
Patrick <zhanghaopai@outlook.com>
2024-07-21 13:15:44 +0800

    新增测试环境代码

---
+++ env_test.py
@@ -0,0 +1,50 @@
+import configparser
+import logging.config
+import os
+
+import torch
+from transformers import pipeline
+
+'''
+使用logging
+'''
+logging.config.fileConfig('config/logging.ini')
+logger = logging.getLogger()
+
+'''
+CUDA报OOM错误
+'''
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+'''
+使用CUDA
+'''
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+logger.info("使用的设备是：%s", device)
+
+
+def main(config):
+    pipe = pipeline(
+        "text-generation",
+        model=config.get("config", "offline_model"),
+        device_map="auto",
+        trust_remote_code=True,
+    )
+    prompt = "Please write a python program that add numbers from 1 to 10."
+
+    outputs = pipe(
+        prompt,
+        max_new_tokens=300,
+        do_sample=True,
+        temperature=0.7,
+        top_k=3,
+        top_p=0.95,
+    )
+    logger.debug(outputs[0]["generated_text"])
+
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+    main(config)
