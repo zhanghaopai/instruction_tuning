20be5312580a0637b5c7a0c2ca6b6b12712d088b
Patrick <zhanghaopai@outlook.com>
2024-07-23 02:51:21 +0800

    提交预测

---
+++ utils/predict.py
@@ -0,0 +1,18 @@
+def predict(messages, model, tokenizer):
+    device = "cuda"
+    text = tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=512)
+    generated_ids = [
+        output_ids[len(input_ids):]
+        for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    print(response)
+
+    return response
