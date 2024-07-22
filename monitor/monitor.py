87840cac432d1b23b5748a0b100bf0dac3f4c71e
Patrick <zhanghaopai@outlook.com>
2024-07-23 01:07:54 +0800

    返回监控配置

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,10 +1,8 @@
 from swanlab.integration.huggingface import SwanLabCallback
 
 
-def load_monitor(config):
+def load_monitor():
     return SwanLabCallback(
-        project="",
-        description="微调",
-        config={
-        }
+        project="instruction-fine-tuning",
+        description="使用twitter情感分类数据微调microsoft/phi-2模型",
     )
