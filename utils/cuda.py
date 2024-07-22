ddd0819beacb74c635e70e4b145956b8e992e8e1
Patrick <zhanghaopai@outlook.com>
2024-07-22 19:41:51 +0800

    提交

--- utils/cuda.py
+++ utils/cuda.py
@@ -4,6 +4,7 @@ import torch
 
 # 环境原因
 # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 for i in range(torch.cuda.device_count()):
     device = torch.device("cuda:" + str(i) if torch.cuda.is_available() else "cpu")
