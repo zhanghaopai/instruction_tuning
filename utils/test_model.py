18ae98d428f319089fbe6a90e8e59d507f545b52
Patrick <zhanghaopai@outlook.com>
2024-07-29 21:42:29 +0800

    提交测试

--- utils/test_model.py
+++ utils/test_model.py
@@ -8,13 +8,13 @@ from transformers import pipeline
 '''
 使用logging
 '''
-logging.config.fileConfig('config/logging.ini')
+logging.config.fileConfig('../config/logging.ini')
 logger = logging.getLogger()
 
 '''
 CUDA报OOM错误
 '''
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 '''
 使用CUDA
 '''
@@ -27,11 +27,14 @@ logger.info("使用的设备是：%s", device)
 def test_env(config):
     pipe = pipeline(
         "text-generation",
-        model=config.get("base", "offline_model"),
+        model="../model/phi-2/",
         device_map="auto",
         trust_remote_code=True,
     )
-    prompt = "Please write a python program that add numbers from 1 to 10."
+    prompt = "You are an expert in sentiment classification. You will receive a piece of text and a list of candidate options, and you are asked to select the sentiment that matches the emotional tendency of the text from the candidates."
+    user = "text:i was feeling a little skeptical that it would arrive on time the situation was not improved by the fact that despite various perfect party setups seeking in ffxi nobody was bothering to set them up including me but duh im lazy, options:['sadness', 'joy', 'love', 'anger', 'fear', 'surprise']"
+    label = "fear"
+
 
     outputs = pipe(
         prompt,
@@ -43,8 +46,7 @@ def test_env(config):
     )
     logger.debug(outputs[0]["generated_text"])
 
-
 if __name__ == "__main__":
     config = configparser.ConfigParser()
-    config.read("config/config.ini")
+    config.read("../config/config.ini")
     test_env(config)
